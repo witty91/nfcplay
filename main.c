a2a743e7b813b00eb8b0fabe9311ffa821a3b37c
Christopher Witt <github@ch-witt.de>
2021-05-29 21:52:00 +0200

    can now read tag, fails quick...need to read constantly and than act on information

--- main.c
+++ main.c
@@ -3,6 +3,18 @@
 #include <nfc/nfc.h>
 #include <usb.h>
 
+static void
+print_hex(const uint8_t *pbtData, const size_t szBytes)
+{
+  size_t  szPos;
+
+  for (szPos = 0; szPos < szBytes; szPos++) {
+    printf("%02x  ", pbtData[szPos]);
+  }
+  printf("\n");
+}
+
+
 int main()
 {
     nfc_device *pnd;
@@ -26,5 +38,27 @@ int main()
   }
 
   printf("NFC reader: %s opened\n", nfc_device_get_name(pnd));
-    return 0;
+      // Poll for a ISO14443A (MIFARE) tag
+  const nfc_modulation nmMifare = {
+    .nmt = NMT_ISO14443A,
+    .nbr = NBR_106,
+  };
+  if (nfc_initiator_select_passive_target(pnd, nmMifare, NULL, 0, &nt) > 0) {
+    printf("The following (NFC) ISO14443A tag was found:\n");
+    printf("    ATQA (SENS_RES): ");
+    print_hex(nt.nti.nai.abtAtqa, 2);
+    printf("       UID (NFCID%c): ", (nt.nti.nai.abtUid[0] == 0x08 ? '3' : '1'));
+    print_hex(nt.nti.nai.abtUid, nt.nti.nai.szUidLen);
+    printf("      SAK (SEL_RES): ");
+    print_hex(&nt.nti.nai.btSak, 1);
+    if (nt.nti.nai.szAtsLen) {
+      printf("          ATS (ATR): ");
+      print_hex(nt.nti.nai.abtAts, nt.nti.nai.szAtsLen);
+    }
+  }
+  // Close NFC device
+  nfc_close(pnd);
+  // Release the context
+  nfc_exit(context);
+  exit(EXIT_SUCCESS);
 }
