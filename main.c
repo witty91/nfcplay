f7e1075d938ea863f4ae7d893391c52acfc5e07b
Christopher Witt <github@ch-witt.de>
2021-07-11 22:37:57 +0200

    actually got this somewhat working now...

--- main.c
+++ main.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
 #include <nfc/nfc.h>
+#include <unistd.h>
 #include <usb.h>
+
+struct idMatchPlaylist {
+    uint64_t uid;
+    char *plname;
+};
+
+typedef struct idMatchPlaylist idMatchPlaylist;
+
+uint64_t hex2int(char *hex) {
+    uint64_t val = 0;
+    while (*hex) {
+        // get current character then increment
+        uint8_t byte = *hex++;
+        // transform hex character to the 4bit equivalent number, using the ascii table indexes
+        if (byte >= '0' && byte <= '9') byte = byte - '0';
+        else if (byte >= 'a' && byte <='f') byte = byte - 'a' + 10;
+        else if (byte >= 'A' && byte <='F') byte = byte - 'A' + 10;
+        // shift 4 to make space for new digit, and add the 4 bits of the new digit
+        val = (val << 4) | (byte & 0xF);
+    }
+    return val;
+}
+
+idMatchPlaylist getidMatchPlaylist(FILE *matchlist){
+    idMatchPlaylist imp;
+    char *line[250];
+    char *uidc;
+    fgets(line, 250, matchlist);
+    printf("%s\n", line);
+    uidc = strtok(line, "\t");
+    //printf("read uid %s\n", uid);
+    imp.plname = strdup(strtok(0, "\t"));
+    //printf("read plname %s\n", plname);
+    imp.uid = hex2int(uidc);
+    return imp;
+}
+int getNumberOfLines(FILE *matchlist){
+    int lines = 0;
+    char ch;
+    while(!feof(matchlist)){
+        ch = fgetc(matchlist);
+        if(ch == '\n')
+        {
+            lines++;
+        }
+    }
+    return lines;
+}
+
 
 static uint64_t
 get_hex(const uint8_t *pbtData, const size_t szBytes)
@@ -15,30 +66,10 @@ get_hex(const uint8_t *pbtData, const size_t szBytes)
     uid = uid * 256;
     //printf("post shift: %x\n",uid);
     uid = uid + pbtData[szPos];
-
   }
 
   return uid;
 }
-
-static void
-read_matchlist(){
-    FILE *matchlist;
-    char *uid, *plname;
-    char *line[250];
-    if ((matchlist = fopen("/opt/nfcplay/matchlist", "r")) == NULL){
-        printf("your matchlist does not exist.\n");
-        exit(1);
-    }
-    while (fgets(line, 250, matchlist)){
-            printf("%s\n", line);
-            uid = strtok(line, " ");
-            printf("read uid %s\n", uid);
-            plname = strtok(0, " ");
-            printf("read plname %s\n", plname);
-    }
-}
-
 
 int main()
 {
@@ -68,18 +99,42 @@ int main()
     .nmt = NMT_ISO14443A,
     .nbr = NBR_106,
   };
-  read_matchlist();
-  while (true){
-  if (nfc_initiator_select_passive_target(pnd, nmMifare, NULL, 0, &nt) > 0) {
-    uint64_t hex = get_hex(nt.nti.nai.abtUid, nt.nti.nai.szUidLen);
-    printf("uid is: %x\n", hex);
-//    print_hex(&nt.nti.nai.btSak, 1);
-    if (nt.nti.nai.szAtsLen) {
-      printf("          ATS (ATR): ");
-
-      //print_hex(nt.nti.nai.abtAts, nt.nti.nai.szAtsLen);
+    int nLines = 0;
+    FILE *matchlist;
+    if ((matchlist = fopen("/opt/nfcplay/matchlist", "r")) == NULL){
+        printf("your matchlist does not exist.\n");
+        exit(1);
     }
-  }
+    nLines = getNumberOfLines(matchlist);
+    idMatchPlaylist matcher[nLines];
+    rewind(matchlist);
+    for (int i = 0; i < nLines; i++){
+        matcher[i] = getidMatchPlaylist(matchlist);
+        printf("received: %014lx and %s\n",matcher[i].uid,matcher[i].plname);
+    }
+    uint64_t oldhex = 0;
+    while (true){
+    if (nfc_initiator_select_passive_target(pnd, nmMifare, NULL, 0, &nt) > 0) {
+        uint64_t hex = get_hex(nt.nti.nai.abtUid, nt.nti.nai.szUidLen);
+        printf("uid is: %lx\n", hex);
+        for (int i = 0; i < nLines; i++){
+            if (matcher[i].uid == hex && oldhex != hex){
+                //printf("Found a match(%014lx), playing %s\n", matcher[i].uid, matcher[i].plname);
+                system("mpc clear");
+                char *command[100];
+                //printf("%s\n", command);
+                snprintf(command, sizeof(command), "mpc load %s", matcher[i].plname);
+                system(command);
+                system("mpc play");
+                oldhex = hex;
+            }else if (oldhex = hex){
+                //do nothing?
+            }else if (hex == "error   libnfc.driver.acr122_usb        Unable to write to USB (Connection timed out)"){
+                system("mpc pause");
+            }
+        }
+    }
+    sleep(1);
   }
   // Close NFC device
   nfc_close(pnd);
